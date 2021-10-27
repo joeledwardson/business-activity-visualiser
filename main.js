05894ade67f760ae41f27c5d2e7f582d2eedd008
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-10-27 13:26:18 +0100

    API hardcoded to table 1 for initial testing

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var express = require("express")
 var app = express()
 var db = require("./database.js");
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Server port
 var HTTP_PORT = 8000 
@@ -18,20 +21,67 @@ app.get("/", (req, res, next) => {
 // Insert here other API endpoints
 app.post("/api/geocodes", (req, res, next) => {
   console.log(req.body);
-  var sql = 'select * from geodata';
-  var params = [];
+  if(!Number.isInteger(req.body.level)) {
+    res.status(400).json({"error": 'expected "level" to be an integer'});
+    return;
+  }
+
+  if(req.body.level <= 0) {
+    res.status(400).json({"error": 'expected "level" to be >= 1'});
+    return
+  }
+  
+  var sql = 'SELECT l.`index`, l.name, r.data FROM geolookups l INNER JOIN geodata r  ON l.`index`=r.`index` WHERE ($lvl >= level and $lvl <= level_end) OR ($lvl > level and has_children = 0);';
+  var params = {
+    "$lvl": req.body.level
+  };
   db.all(sql, params, (err, rows) => {
     if (err) {
       res.status(400).json({"error":err.message});
       return;
+    } 
+
+    if(rows.length == 0) {
+      res.status(400).json({"error": "no rows found"});
+      return;
     }
-    res.json({
-        "message":"success",
-        "data": rows.map(row => ({
-          index: row.index,
-          data: JSON.parse(row.data)
-        }))
-    })
+
+    let features = rows.map(row => ({
+      ...JSON.parse(row.data),
+      properties: {
+        name: row.name,
+        id: row.index,
+      }
+    }));
+
+    const setProperties = (index) => {
+      var feature = features[index];
+      var isql = `SELECT variable_1, value FROM tabledata WHERE \`table\`=1 AND variable_x="${feature.properties.id}";`;
+      var iparams = [];
+      db.all(isql, iparams, (ierr, irows) => {
+        if (ierr) {
+          res.status(400).json({"error":ierr.message});
+          return;
+        }
+        irows.forEach(irow => {
+          feature.properties[irow.variable_1] = irow.value;
+        });
+        if(index + 1 < features.length) {
+          setProperties(index+1);
+        } else {
+          res.json({
+            "message":"success",
+            "data": {
+              type: 'FeatureCollection',
+              features
+            }
+          });
+        }
+      });
+    }
+
+    setProperties(0);
+
   });
 })
 
